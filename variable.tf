0bfbaeeccd4ece2d8f434dc1b25491571d5dffaa
srdangat <sanketdangat9@gmail.com>
2025-03-15 20:18:31 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "key_name" {
+  default = "mumabi"
+}
+
+variable "key_path" {
+  default = "C:/Users/sanke/Downloads/mumbai.pem"
+}
+
+variable "instance_type" {
+  default = "t2.medium"
+}
+
+variable "ami" {
+  default = "ami-04b4f1a9cf54c11d0"
+}
